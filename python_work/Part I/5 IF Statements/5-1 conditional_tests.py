305117a0cfbc24a6ac56435843d42cba461ab3d1
Richard Johnson <richard.johnson@zeninternet.co.uk>
2024-11-14 22:10:40 +0000

    if statements

---
+++ python_work/Part I/5 IF Statements/5-1 conditional_tests.py
@@ -0,0 +1,40 @@
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
+
+print("\nIs car == 'Subaru'? I predict False.")
+print(car == 'Subaru')
+
+print("\nIs car == 'SUBARU'? I predict False.")
+print(car == 'SUBARU')
+
+print("\nIs car == 'Subaru' or car == 'subaru'? I predict True.")
+print(car == 'Subaru' or car == 'subaru')
+
+print("\nIs car == 'SUBARU'? I predict True.")
+print(car.upper() == 'SUBARU')
+
+# Do the same as above but for != and not
+print("\nIs car != 'subaru'? I predict False.")
+print(car != 'subaru')
+
+print("\nIs car != 'audi'? I predict True.")
+print(car != 'audi')
+
+print("\nIs car != 'Subaru'? I predict True.")
+print(car != 'Subaru')
+
+print("\nIs car != 'SUBARU'? I predict True.")
+print(car != 'SUBARU')
+
+print("\nIs car != 'Subaru' or car != 'subaru'? I predict False.")
+print(car != 'Subaru' or car != 'subaru')
+
+print("\nIs car != 'SUBARU'? I predict False.")
+print(car.upper() != 'SUBARU')
+
+print("\nIs car != 'SUBARU'? I predict False.")
+print(car.upper() != 'SUBARU')
